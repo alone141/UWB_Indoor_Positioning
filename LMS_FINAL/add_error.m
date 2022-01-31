e44fab57ddf93ff91ddf33772238b7d278032ad5
Melikşah Sagun <68744286+alone141@users.noreply.github.com>
2022-01-31 15:14:19 +0300

    Error ekleme

--- LMS_FINAL/add_error.m
+++ LMS_FINAL/add_error.m
@@ -2,7 +2,6 @@ function output= add_error(input,stddev)
 %Input vektörüne stddev zero mean gaussian ekler
 mean = 0;
 % stddev = 0.2;
-% mean = 0;
 % stddev = 0.52;
 output= input + randn([1 length(input)]).*stddev + mean;
 end
